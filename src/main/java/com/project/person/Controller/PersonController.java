d348a62d4ae7bd1507c43f3ed15963338c8b7967
kaweny <kawenyv@gmail.com>
2022-10-13 23:45:45 -0300

    Adicao de tratamento de erro na person

--- src/main/java/com/project/person/Controller/PersonController.java
+++ src/main/java/com/project/person/Controller/PersonController.java
@@ -1,11 +1,16 @@
 package com.project.person.Controller;
 
+import com.project.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.project.model.Person;
 import com.project.person.service.PersonService;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class PersonController {
@@ -18,23 +23,41 @@ public class PersonController {
     }
     @GetMapping("/person/{idPerson}")
     public Person getById(@PathVariable("idPerson") long idPerson) {
-        Person response = personService.getById(idPerson);
-        return response;
+        try {
+            return personService.getById(idPerson);
+        } catch (NoSuchElementException exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado", exception);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Problema na requisicao", exception);
+        }
     }
 
     @PostMapping("/person")
     public Person create(@RequestBody final Person person) {
-        return personService.create(person);
+        try {
+            return personService.create(person);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Problema na requisicao", exception);
+        }
     }
     @PutMapping("/person")
     public Person update(@RequestBody Person person) {
-        Person response = personService.update(person);
-        return response;
+        try {
+            return personService.update(person);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Problema na requisicao", exception);
+        }
     }
     @DeleteMapping("/person/{idPerson}")
         public void delete(@PathVariable("idPerson") long idPerson){
-       personService.delete(idPerson);
+        try {
+            personService.delete(idPerson);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado", exception);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Problema na requisicao", exception);
+        }
     }
 
-// TODO: adicionar get list
+
 }
