ceb73997a4a8ec4ee1385b747ec14573af7b4277
Vitor Lichoti <vitorlichoti@pop-os.localdomain>
2022-10-25 11:04:06 -0400

    Teste na Movement: getByID_success

---
+++ src/test/java/com/project/movement/controller/MovementControllerTest.java
@@ -0,0 +1,45 @@
+package com.project.movement.controller;
+
+import com.project.model.Movement;
+import com.project.movement.service.MovementService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+import java.util.NoSuchElementException;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MovementControllerTest {
+    @InjectMocks
+    private MovementController controller;
+
+    @Mock
+    private MovementService service;
+
+    @Test
+    public void getByID_success() {
+        when(service.getById(anyLong())).thenReturn(buildMovement());
+        Movement response = controller.getById(1L);
+        assertNotEquals(null, response); // caso id seja null
+        assertEquals(1L, response.getId());
+        assertEquals(1L, response.getCreationPersonId());
+        assertEquals(1L, response.getProductId());
+        assertEquals(2L, response.getQuantity());
+    }
+
+    public Movement buildMovement() {
+        return Movement.builder()
+                .id(1L)
+                .creationPersonId(1L)
+                .productId(1L)
+                .quantity(2L)
+                .build();
+    }
+}
