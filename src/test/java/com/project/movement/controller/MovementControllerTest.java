5bf7ae59a482e39ea2de32efa42804b8207ed683
Vitor Lichoti <vitorlichoti@pop-os.localdomain>
2022-10-28 09:17:57 -0400

    implementa testes "not_found" na movement controller

--- src/test/java/com/project/movement/controller/MovementControllerTest.java
+++ src/test/java/com/project/movement/controller/MovementControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
 import java.util.NoSuchElementException;
@@ -32,6 +33,20 @@ public class MovementControllerTest {
         assertEquals(1L, response.getCreationPersonId());
         assertEquals(1L, response.getProductId());
         assertEquals(2L, response.getQuantity());
+    }
+
+    @Test
+    public void getByID_notFound() {
+        when(service.getById(anyLong())).thenThrow(NoSuchElementException.class);
+        Throwable throwable = assertThrows(
+                ResponseStatusException.class,
+                () -> controller.getById(1L),
+                "Movimento nao encontrado"
+        );
+        System.out.println(throwable);
+        assertEquals(ResponseStatusException.class, throwable.getClass());
+        assertNotEquals(NoSuchElementException.class, throwable.getClass());
+        assertEquals("404 NOT_FOUND \"Produto nao encontrado\"", throwable.getMessage());
     }
 
     public Movement buildMovement() {
