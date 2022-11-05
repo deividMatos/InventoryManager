b847eaff8b2bcb2b6443c2e465b0abbe2fa164fc
Deivid Matos <deivid.dgm@gmail.com>
2022-11-05 14:34:17 -0300

    tratamento de metodos de upodate para nao atualizar qnd nao existir

--- src/main/java/com/project/model/MovementType.java
+++ src/main/java/com/project/model/MovementType.java
@@ -6,10 +6,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
 import java.sql.Date;
 
 @Entity
 @Table(name = "movement_type")
+@Data
 public class MovementType {
     @Id
     @Column(name = "id")
